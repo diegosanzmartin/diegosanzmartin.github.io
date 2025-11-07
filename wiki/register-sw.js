5140cd9bd09e3a92d3518cb2136e00822e177bd6
Diego Sanz Martín <diego.sanz@holafly.com>
2025-11-07 09:36:50 +0000

    fix: service-worker

--- wiki/register-sw.js
+++ wiki/register-sw.js
@@ -1,6 +1,5 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
     navigator.serviceWorker.register('/wiki/service-worker.js')
       .then(registration => {
         console.log('Service Worker registrado con éxito:', registration);
